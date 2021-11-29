a25c9c20dbe5416ca116f451357e1e93bad753d2
alex99 <prodanalexandru580@gmail.com>
2021-11-29 13:52:48 +0300

    inheritance example

--- python-essential-training/8_classes/basic.py
+++ python-essential-training/8_classes/basic.py
@@ -1,20 +1,37 @@
 class Animal:
     static_arr = [1,2,3]
     def __init__(self, **kargs):
-        self._type = kargs['type'] if 'type' in kargs else 'generic_animal'
-        self._name = kargs['name'] if 'name' in kargs else 'generic_name'
-        self._sound = kargs['sound'] if 'sound' in kargs else 'generic sound'
+        if 'type' in kargs:
+             self._type = kargs['type']
+        if 'name' in kargs:
+            self._name = kargs['name']
+        if 'sound' in kargs:
+            self._sound = kargs['sound']
         
     def type(self, t=None):
         if t: self._type = t
-        return self._type
+        try:
+            return self._type
+        except AttributeError:
+            return None
     def name(self, n=None):
         if n: self._name = n
-        return self._name
+        try:
+            return self._name
+        except ArithmeticError:
+            return None
     def sound(self, s):
         if s: self._sound = s
-        return self._sound
+        try:
+            return self._sound
+        except AttributeError:
+            return None
     
+class Duck(Animal):
+    def __init__(self, **kargs):
+        self._type = 'duck'
+        if 'type' in kargs: del kargs['type']
+        super().__init__(**kargs)
 
 def main():
     my_animal = Animal(type="pig", name="george", sound="grwwow")
@@ -26,6 +43,10 @@ def main():
     print (my_generic_animal.static_arr)
     
     my_animal.name('alex')
-    print(my_animal.name())   
+    print(my_animal.name())
+    
+    duck = Duck(name="fred", sound="whoack")
+    
+    print(duck.type())   
 if __name__ == '__main__':
     main()
