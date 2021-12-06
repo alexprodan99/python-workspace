b49fa392c7278d52e483d4a87c455b68a87adeae
alex99 <prodanalexandru580@gmail.com>
2021-12-06 12:23:17 +0300

    default values ex4

--- python-object-oriented-programming/data-clasess/default_values_example.py
+++ python-object-oriented-programming/data-clasess/default_values_example.py
@@ -1,11 +1,14 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+
 
 @dataclass
 class Book:
     title: str = 'No title'
     author: str = 'No author'
     pages: int = 123
-    price: float = 12
+    price: float = field(default=12)
 
 book = Book()
 print(book)
+book2 = Book(title="War and peace")
+print(book2)
