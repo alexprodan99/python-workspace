3176dd917b5e9bffee74fb6109716e40a5140a58
alex99 <prodanalexandru580@gmail.com>
2021-12-06 12:20:10 +0300

    default values ex2

--- python-object-oriented-programming/data-clasess/default_values_example.py
+++ python-object-oriented-programming/data-clasess/default_values_example.py
@@ -2,11 +2,11 @@ from dataclasses import dataclass
 
 @dataclass
 class Book:
-    title: str
-    author: str
-    pages: int = 321
-    price: float = 123.2
+    # error here because after an arg with default value there is an arg without a default value
+    title: str = 'No title'
+    author: str = 'No author'
+    pages: int
+    price: float
 
- 
-book = Book('War and peace','Lev Tolstoi')
+book = Book(pages=123,price=123)
 print(book)
