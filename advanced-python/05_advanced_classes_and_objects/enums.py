f65233cdcbdd0f7ca6afb846db31caccaa6fc4c3
alex99 <prodanalexandru580@gmail.com>
2021-12-06 17:11:19 +0300

    enums auto

--- advanced-python/05_advanced_classes_and_objects/enums.py
+++ advanced-python/05_advanced_classes_and_objects/enums.py
@@ -1,11 +1,13 @@
 
-from enum import Enum, unique
+from enum import Enum, unique, auto
 
 @unique
 class Fruit(Enum):
     APPLE = 1
     BANANA = 2
     ORANGE = 3
+    # if you don't care what value to put you can use auto => basically it will get last value used + 1 (if auto is for first item it will be 0 + 1 = 1)
+    PEAR = auto()
 
 
 def main():
@@ -16,7 +18,8 @@ def main():
     print(Fruit.APPLE.name) # APPLE
     print(Fruit.APPLE.value) # 1
     # you cannot have duplicate keys in Fruits (you can have duplicate values if you are not having unique decorator!)
-    
+    print(Fruit.PEAR.name) # PEAR
+    print(Fruit.PEAR.value) # 4
 
 if __name__ == '__main__':
     main()
