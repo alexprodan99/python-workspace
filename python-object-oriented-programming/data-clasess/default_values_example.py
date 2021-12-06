6fa258d66e3894f415542d013cb2c5eaf745b76e
alex99 <prodanalexandru580@gmail.com>
2021-12-06 12:25:27 +0300

    default values using a function

--- python-object-oriented-programming/data-clasess/default_values_example.py
+++ python-object-oriented-programming/data-clasess/default_values_example.py
@@ -1,12 +1,17 @@
 from dataclasses import dataclass, field
+import random
 
+def price_func():
+    return float(random.randrange(20,40))
 
 @dataclass
 class Book:
     title: str = 'No title'
     author: str = 'No author'
     pages: int = 123
-    price: float = field(default=12)
+    price: float = field(default_factory=price_func)
+    
+    
 
 book = Book()
 print(book)
